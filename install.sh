ca04eb5958033cbfdf86d2411b47be4e8e3ac965
Nick Matthews <nick@linuxpanic.com>
2023-03-18 02:21:21 +0000

    Rewritten Installer

--- install.sh
+++ install.sh
@@ -6,8 +6,11 @@ sudo apt install cinnamon-desktop-environment lightdm
 
 unzip Windows-XP-Desktops.zip && unzip Windows-XP-3.1.zip
 cd
-cd /$HOME
+cd $HOME
 mkdir .icons
 mkdir .themes
-mv /$HOME/Desktop/UbuntuXP/Windows-XP-Desktops /$HOME/.themes
-mv /$HOME/Desktop/UbuntuXP/Windows-XP-3.1 /$HOME/.icons
+pwd
+cd Desktop/LinuxXP
+pwd
+sudo mv Windows-XP-Desktops/ $HOME/.themes
+sudo mv Windows-XP-3.1/ $HOME/.icons
